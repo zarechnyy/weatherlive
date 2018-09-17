f919869ac546b2a4438f847c587d2e39ecb0d0aa
я <yaroslavzarechnyy@yaroslavs-macbook-pro.local>
2018-09-17 17:43:04 +0300

    Initial Commit

---
+++ weatherlive/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  weatherlive
+//
+//  Created by Yaroslav Zarechnyy on 9/17/18.
+//  Copyright © 2018 Yaroslav Zarechnyy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
